d7c6811ecbca6bbd87dec20bc431da85f2a9fc28
Ivan-4884 <iv.dushkin@gmail.com>
2020-12-17 13:57:55 +0300

    FitTest

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -18,7 +18,7 @@ public class FitTest {
     @Test
     public void whenWomen170Then80() {
         short in = 170;
-        double expected = 80;
+        double expected = 69;
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }

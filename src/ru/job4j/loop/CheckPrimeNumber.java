a574f1634e1abd95ed04e68302d2b85c56b3d0c0
Ivan-4884 <iv.dushkin@gmail.com>
2020-12-26 22:13:09 +0300

    CheckPrimeNumber

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
+
+        for (int num = 2; num < number; num++) {
+            if ((number % num) == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

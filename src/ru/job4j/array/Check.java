348f27954b1db78bbb43eded2be6775b7e80126e
Ivan-4884 <iv.dushkin@gmail.com>
2021-01-14 15:09:22 +0300

    Check

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
-        boolean result = true;
+        boolean result = false;
         for (int i = 0; i < data.length - 1; i++) {
-            if (data[i] == data[i + 1]) {
+            if (data[i] != data[i + 1]) {
                 break;
             } else {
-                result = false;
+                result = true;
             }
         }
         return result;

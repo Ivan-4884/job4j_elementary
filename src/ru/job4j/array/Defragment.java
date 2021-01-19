df01e0ed72b6827d2a82a6579583d985a4d2f45e
Ivan-4884 <iv.dushkin@gmail.com>
2021-01-19 16:08:00 +0300

    Defragment

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int nullIndex = 0; nullIndex < array.length; nullIndex++) {
+            if (array[nullIndex] == null) {
+                for (int notNullIndex = nullIndex + 1; notNullIndex < array.length; notNullIndex++) {
+                    if (array[notNullIndex] != null) {
+                        //мотод SwitchArray.swap для int был реализован. но написание метода для замен ячеек массива из строк вроде не было в упражнениях
+                        array[nullIndex] = array[notNullIndex];
+                        array[notNullIndex] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}

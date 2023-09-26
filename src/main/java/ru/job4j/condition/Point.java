e24860e300689c2989d718d8cd8d9f30d67ee933
Ivan-4884 <iv.dushkin@gmail.com>
2023-09-26 22:28:50 +0300

    9.3

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -12,7 +12,7 @@ public class Point {
     }
 
     public double distance(Point that) {
-        return sqrt(pow(this.x - that.x,2) + pow(this.y - that.y,2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
@@ -22,14 +22,4 @@ public class Point {
         System.out.println(rsl);
 
     }
-
-    /*public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-    }*/
 }

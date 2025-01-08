3faf8c62c21ff167c69695bfd93ba10a90794007
Ivan-4884 <iv.dushkin@gmail.com>
2025-01-08 19:46:58 +0300

    6. Правила Checkstyle [#504981 #502969]

---
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.checkstyle;
+
+class Broken {
+    public static final String NEWVALUE = "";
+
+    private int sizeofempty = 10;
+
+    private String surname;
+
+    private String name;
+
+    private String name2;
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}

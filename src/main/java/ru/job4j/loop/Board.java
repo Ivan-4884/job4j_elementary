41742c621996f86c60c81c2897324b8d7f545889
Ivan-4884 <iv.dushkin@gmail.com>
2025-03-09 12:26:51 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -1,22 +1,18 @@
 package ru.job4j.loop;
 
 public class Board {
-    public static void paint(int width, int height) {
+    public static void paint(int width, int height, char symbol1, char symbol2) {
         for (int row = 0; row < height; row++) {
             for (int cell = 0; cell < width; cell++) {
-                if ((row + cell) % 2 == 0) {
-                    System.out.print("X");
-                } else {
-                    System.out.print(" ");
-                }
+                System.out.print((row + cell) % 2 == 0 ? symbol1 : symbol2);
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        paint(3, 3);
+        paint(3, 3, 'X', ' ');
         System.out.println();
-        paint(4, 4);
+        paint(4, 4, '#', '.');
     }
 }
